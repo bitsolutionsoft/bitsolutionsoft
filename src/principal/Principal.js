e7df34a71f1b6fe169272ba25d0ecd743a2d80b6
unknown <jrenoj62@gmail.com>
2021-02-08 15:56:26 -0600

    primer commit

---
+++ src/principal/Principal.js
@@ -0,0 +1,86 @@
+import logo from '../assets/img/logo.svg';
+import fon from '../assets./img/codesk.png';
+import './../css/principal.css';
+
+function Principal() {
+  return (
+    <div>
+    <div className="Menu">
+         <p className="Title">Codesk</p> 
+        <ul>
+  
+  <li><a href="#Quienes" className="active">¿Quienes somos?</a></li>
+  <li><a href="#Productos"  className="active">Productos</a></li>
+  <li><a href="#Servicios" className="active">Servicios</a></li>
+  <li><a href="#Contactos" className="active">Contactos</a></li>
+</ul>
+
+
+        </div>  
+    <div className="App" id="Quienes">    
+      <div className="Quienes">
+      <p className="SeccTitle">
+         ¿Quiénes Somos? 
+        </p>
+        <p className="Desc">
+          Somos una empresa dedicado al desarrollo de sistema 
+          informatico, sitio web. 
+        </p>
+         </div>
+      </div>   
+
+      <div className="Producto" id="Productos">
+        <img src={logo} className="App-logo" alt="logo" />
+         <p className="SeccTitle">
+         Productos
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+      <div className="Servicios" id="Servicios">
+        <img src={logo} className="App-logo" alt="logo" />
+         <p className="SeccTitle">
+       Servicios
+        
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+      <div className="Contactos" id="Contactos">
+        <img src={codesk} className="App-logo" alt="logo" />
+         <p className="SeccTitle">
+        Contactos
+        
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+       
+       
+       
+   
+   
+    </div>
+  );
+}
+
+export default Principal;
